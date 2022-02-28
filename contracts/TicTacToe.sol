1bb2aad8eff1034601a1598a0d437f26cd8dfb2d
pierredm1 <66529459+pierredm1@users.noreply.github.com>
2022-02-28 14:53:55 +0100

    pierre code

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,6 +1,4 @@
-//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.4.24;
-
 /**
  * @title TicTacToe contract
  **/
@@ -31,6 +29,11 @@ contract TicTacToe {
      */
     uint[9] private board;
 
+    // this private variable records the number of moves that have been played
+    // and is used for checking the current status of the board and the draw
+    uint private n_moves = 0;
+
+
     /**
       * @dev Deploy the contract to create a new game
       * @param opponent The address of player2
@@ -46,9 +49,11 @@ contract TicTacToe {
       * @param a position a
       * @param b position b
       * @param c position c
-      **/    
+      **/
+
     function _threeInALine(uint a, uint b, uint c) private view returns (bool){
         /*Please complete the code here.*/
+        return (board[a] == board[b]) && (board[b] == board[c]);
     }
 
     /**
@@ -58,7 +63,30 @@ contract TicTacToe {
      */
     function _getStatus(uint pos) private view returns (uint) {
         /*Please complete the code here.*/
-        return status;
+
+        // VERTICAL COMBINATION
+        uint ver_pos = pos % 3;
+        if(_threeInALine(ver_pos,ver_pos+3,ver_pos+6)){
+            return board[pos];
+        }
+        // HORIZONTAL COMBINATION
+        uint hori_pos = pos / 3;
+        if(_threeInALine(pos, hori_pos*3+(pos+1)%3, hori_pos*3+(pos+2)%3)){
+            return board[pos];
+        }
+
+        //DIAGONAL COMBINATION
+        if(pos%2==0 && board[4]!=0){
+          if(_threeInALine(0, 4, 8) || _threeInALine(2, 4, 6))
+          return board[pos];
+        }
+
+        // board is full (9)
+        if(n_moves>=9)
+          return 3;
+
+        //if any other case then game is ongoing so 0
+        return 0;
     }
 
     /**
@@ -68,6 +96,12 @@ contract TicTacToe {
      */
     modifier _checkStatus(uint pos) {
         /*Please complete the code here.*/
+        //check the status is ongoing, default value of uint is 0
+        require(status== 0);
+        _;
+        n_moves+=1;
+        //update status
+        status = _getStatus(pos);
     }
 
     /**
@@ -75,7 +109,8 @@ contract TicTacToe {
      * @return true if it's msg.sender's turn otherwise false
      */
     function myTurn() public view returns (bool) {
-       /*Please complete the code here.*/
+        /*Please complete the code here.*/
+        return players[turn-1] == msg.sender;
     }
 
     /**
@@ -83,7 +118,15 @@ contract TicTacToe {
      * update the turn after a move
      */
     modifier _myTurn() {
-      /*Please complete the code here.*/
+        /*Please complete the code here.*/
+        require(myTurn());
+        _;
+        //update turn
+        if(turn == 1){
+            turn = 2;
+        }else{
+            turn = 1;
+        }
     }
 
     /**
@@ -93,6 +136,10 @@ contract TicTacToe {
      */
     function validMove(uint pos) public view returns (bool) {
       /*Please complete the code here.*/
+        if(board[pos] == 1 || board[pos] == 2){
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -100,7 +147,12 @@ contract TicTacToe {
      * @param pos the position the player places at
      */
     modifier _validMove(uint pos) {
-      /*Please complete the code here.*/
+        /*Please complete the code here.*/
+        require(pos>=0);
+        require(pos<9);
+        require(board[pos]!= 1);
+        require(board[pos]!= 2);
+        _;
     }
 
     /**
@@ -116,6 +168,6 @@ contract TicTacToe {
      * @return board
      */
     function showBoard() public view returns (uint[9]) {
-      return board;
+        return board;
     }
 }
